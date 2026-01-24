9e56f75d740ed0e74ebd624874b4accb3f47ad42
Sergey <63001821+sergeykuznetsov1995@users.noreply.github.com>
2026-01-24 23:24:27 +0300

    chore: remove old file sql/init-metastore.sql

--- sql/init-metastore.sql
+++
@@ -1,4 +0,0 @@
--- Create metastore database for Hive (PostgreSQL syntax)
--- Note: This runs only on first PostgreSQL initialization
-CREATE DATABASE metastore;
-GRANT ALL PRIVILEGES ON DATABASE metastore TO airflow;
